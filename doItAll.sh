7d6ecc34ba751eed6435c6898e7b668cf7056559
Sufyaan Kazi <skazi@pivotal.io>
2016-06-22 23:40:13 +0100

    Tidied up delete scripts

--- doItAll.sh
+++ doItAll.sh
@@ -19,9 +19,9 @@ trap 'abort $LINENO' 0
 SECONDS=0
 SCRIPTNAME=`basename "$0"`
 
-./deleteAllApps.sh &
+./deleteListedApps.sh &
 wait
-./deleteAllServices.sh &
+./deleteListedServices.sh &
 wait
 sh ./1_createServices.sh
 sh ./2_build.sh
