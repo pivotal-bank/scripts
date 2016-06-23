02cad21e5dcdb88542c92c33bcea7a5c6b8880b2
Sufyaan Kazi <skazi@pivotal.io>
2016-06-23 17:09:26 +0100

    Removed PWS check

--- 1_createServices.sh
+++ 1_createServices.sh
@@ -62,16 +62,6 @@ create_all_services()
 
 main()
 {
-  # Work out the CF_TARGET
-  CF_TARGET=`cf target | grep "API" | cut -d" " -f5| xargs`
-  # Disable PWS because of SCS Tile
-  PWS=`echo $CF_TARGET | grep "run.pivotal.io" | wc -l`
-  if [ $PWS -ne 0 ]
-  then
-    echo_msg "This won't run on PWS, please use another environment"
-    exit 1
-  fi
-
   create_all_services
   summaryOfServices
 }
